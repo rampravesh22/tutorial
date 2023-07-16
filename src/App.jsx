c251e085bb9e858cf9c60db50d426f8e28789814
rpc1752 <rpc1752@gmail.com>
2023-07-16 16:08:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Image from "./components/Image";
 function App() {
+	const arr = [
+		800, 801, 802, 802, 885, 885, 775, 885, 883, 774, 888, 888, 783, 853, 885,
+		834,
+	];
 	return (
 		<>
-			<div>Main</div>
+			<div className="w-full min-h-screen flex flex-wrap justify-center gap-12 mt-24 transition-all duration-500">
+				{arr.map((value, ind) => {
+					return <Image key={ind} value={value} />;
+				})}
+			</div>
 		</>
 	);
 }
