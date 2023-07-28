a15b4e22f6ab6d9b1ab30a27d087d7e91c922c35
rpc1752 <rpc1752@gmail.com>
2023-07-28 16:52:46 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+// main.js or App.js
+import React, { useEffect } from "react";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import AnimatedBox from "./components/AnimatedBox";
+
 function App() {
-	return (
-		<>
-			<div>Main</div>
-		</>
-	);
+	useEffect(() => {
+		Aos.init({
+			duration: 800,
+			offset: 100,
+			easing: "ease-in-out",
+		});
+	}, []);
+
+	// Your other React components and code go here
+
+	return <AnimatedBox />;
 }
 
 export default App;
---
+++ src/components/AnimatedBox.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+const AnimatedBox = () => {
+	useEffect(() => {});
+	return (
+		<div>
+			<div className="h-60 border-2 w-1/2">
+				<div className="w-[90%] bg-red-500 h-20 ml-auto mr-auto"></div>
+			</div>
+		</div>
+	);
+};
+
+export default AnimatedBox;
