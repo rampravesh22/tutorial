00735639de8bee63667027f6670909616e64aa10
rpc1752 <rpc1752@gmail.com>
2023-08-01 09:51:21 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
 	return (
 		<>
-			<div>Main</div>
+			<div className="flex flex-col min-h-screen">
+				<div className="bg-blue-950 text-white h-12">Header</div>
+				<div className="bg-red-600 flex-grow">Body</div>
+				<div className="bg-yellow-950 text-white h-40">Footer</div>
+			</div>
 		</>
 	);
 }
