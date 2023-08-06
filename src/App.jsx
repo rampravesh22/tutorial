17068e53af82dce87ff53ce0f4c27ccaa968e172
rpc1752 <rpc1752@gmail.com>
2023-08-06 15:48:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 function App() {
+	const [notes, setNotes] = useState([
+		{
+			id: Date.now(),
+			content: "hello",
+		},
+		{
+			id: Date.now(),
+			content: "find the data",
+		},
+		{
+			id: Date.now(),
+			content: "find the data",
+		},
+	]);
+	useEffect(() => {
+		const getNotes = localStorage.getItem("note");
+		if (getNotes) {
+			setNotes(JSON.parse(getNotes));
+		}
+		console.log(getNotes);
+	}, []);
 	return (
 		<>
 			<div>Main</div>
