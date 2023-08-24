646fe7fa2d8bf8d8a8cdd529d8c2a50cc706a3d6
rpc1752 <rpc1752@gmail.com>
2023-08-24 08:00:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 function App() {
 	return (
 		<>
-			<div>Main</div>
+			<div className="flex bg-slate-500 h-screen flex-col">
+				<header className="h-14 bg-white w-full flex justify-between items-center px-3 md:px-10">
+					<div className="logo text-3xl font-extrabold text-slate-800">
+						Tino
+					</div>
+					<nav>
+						<ul className="flex md:gap-8 gap-2">
+							<li>Home</li>
+							<li>Contact</li>
+							<li>About</li>
+							<li>Sign-Up</li>
+							<li>Login</li>
+						</ul>
+					</nav>
+				</header>
+				<main className="flex-grow flex">
+					<div className="sidbar bg-red-600 hidden md:block md:w-[30%] h-full"></div>
+					<div className="content bg-blue-700 w-full md:w-[70%] h-full"></div>
+				</main>
+				<footer className="w-full h-28 bg-green-700"></footer>
+			</div>
 		</>
 	);
 }
