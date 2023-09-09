20fe29ed230e90ba1873a576242c154c4f0e30ed
rpc1752 <rpc1752@gmail.com>
2023-09-09 19:53:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function App() {
+	const [toggle, setToggle] = useState(false);
+	const [name, setNmae] = useState("");
 	return (
 		<>
-			<div>Main</div>
+			<div className={`bg-blue-700 px-2 sm:px-6 ${} text-white p-4 flex justify-between`}>
+				<button
+					onClick={() => setToggle(!toggle)}
+					className={`w-8 h-8 bg-pink-800 sm:hidden rounded-md ${
+						toggle ? "ring" : ""
+					}`}
+				></button>
+			</div>
 		</>
 	);
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,jsx,html}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
