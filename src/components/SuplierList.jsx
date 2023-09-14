fd81f9104e68fefa95213d610d41357daa45ed3e
rpc1752 <rpc1752@gmail.com>
2023-09-14 16:42:31 +0530

    updated

--- src/components/SuplierList.jsx
+++ src/components/SuplierList.jsx
@@ -1,20 +1,22 @@
-// SupplierList.js
 import React, { useEffect, useState } from "react";
 import SupplierCard from "./SuplierCard";
 import axios from "axios";
-
+import Error from "./Error";
 const BASE_URL = "https://staging.iamdave.ai";
 
 function SupplierList() {
+	const [loading, setLoading] = useState(false);
 	const [suppliers, setSuppliers] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [isLastPage, setIsLastPage] = useState(false);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		fetchSuppliers(currentPage);
 	}, [currentPage]);
 
 	const fetchSuppliers = async (pageNumber) => {
+		setLoading(true);
 		try {
 			const res = await axios.get(
 				`${BASE_URL}/list/supply?_page_number=${pageNumber}`,
@@ -29,8 +31,11 @@ function SupplierList() {
 			);
 			setSuppliers(res.data.suppliers); // Update the state with the suppliers data
 			setIsLastPage(res.data.is_last); // Fix the variable assignment
+			setLoading(false);
 		} catch (error) {
 			console.log("errors", error);
+			setError(true);
+			setLoading(false);
 		}
 	};
 
@@ -42,17 +47,26 @@ function SupplierList() {
 
 	return (
 		<div>
-			{/* Render supplier cards here */}
-			<div className="supplier-container">
-				{suppliers.map((supplier) => (
-					<SupplierCard key={supplier.id} supplier={supplier} />
-				))}
-			</div>
+			{loading ? (
+				<div className="loader-wrapper">
+					<div className="loader1"></div>
+				</div>
+			) : null}
+			{!error ? (
+				<div>
+					<div className="supplier-container">
+						{suppliers.map((supplier) => (
+							<SupplierCard key={supplier.id} supplier={supplier} />
+						))}
+					</div>
 
-			{/* Pagination controls */}
-			<div className="pagination">
-				<button onClick={nextPage}>Next Page</button>
-			</div>
+					<div className="pagination">
+						<button onClick={nextPage}>Next Page</button>
+					</div>
+				</div>
+			) : (
+				<Error />
+			)}
 		</div>
 	);
 }
