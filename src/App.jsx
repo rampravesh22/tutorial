181f51d1ed08fc316b3d0e5516555ecaee7c464b
rpc1752 <rpc1752@gmail.com>
2023-09-15 10:22:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./components/Home";
+import Header from "./components/Header";
 function App() {
 	return (
 		<>
-			<div>hello</div>
+			<ChakraProvider>
+				<Header />
+			</ChakraProvider>
 		</>
 	);
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
+
+const Home = () => {
+	return (
+		<div>
+			<Spinner size={"xl"} />
+		</div>
+	);
+};
+
+export default Home;
