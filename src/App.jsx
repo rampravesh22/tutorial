44a0b1f0b6cdd4fd7d50e0fe6189a444c0e6d3b8
rpc1752 <rpc1752@gmail.com>
2023-09-25 11:51:14 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Posts from "./Posts";
 
 const App = () => {
 	const [posts, setPosts] = useState(data.comments);
+	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
 	const itemsPerPage = 9;
 
@@ -11,17 +12,36 @@ const App = () => {
 	const endIndex = startIndex + itemsPerPage;
 	const currentPosts = posts.slice(startIndex, endIndex);
 	const totalPages = Math.ceil(posts.length / itemsPerPage);
+
+	const handlePrev = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setCurrentPage(currentPage - 1);
+			setLoading(false);
+		}, 400);
+	};
+	const handleNext = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setCurrentPage(currentPage + 1);
+			setLoading(false);
+		}, 400);
+	};
 	return (
 		<>
-			<div className="app-wrapper flex gap-4 content-center h-screen flex-wrap justify-center ">
-				<div className="posts gap-2 flex flex-wrap justify-center">
-					<Posts posts={currentPosts} />
+			<div className="app-wrapper flex gap-4 items-center flex-col content-center p-10 h-screen flex-wrap justify-center ">
+				<div className="posts gap-2 flex-grow content-center flex  flex-wrap justify-center min-h-[400px] max-w-3xl">
+					{!loading ? (
+						<Posts posts={currentPosts} />
+					) : (
+						<div className="w-20 h-20 border-4 border-e-black animate-spin  rounded-full"></div>
+					)}
 				</div>
 				<div className="paginate flex gap-3">
 					<button
 						type="button"
 						disabled={currentPage == 1}
-						onClick={() => setCurrentPage(currentPage - 1)}
+						onClick={handlePrev}
 						className="border-2 px-5 py-1 disabled:bg-gray-500 disabled:text-white hover:bg-slate-300 rounded-md"
 					>
 						Prev
@@ -31,7 +51,7 @@ const App = () => {
 					</span>
 					<button
 						type="button"
-						onClick={() => setCurrentPage(currentPage + 1)}
+						onClick={handleNext}
 						className="border-2 px-5 py-1 hover:bg-slate-300  disabled:bg-gray-500 disabled:text-white  rounded-md"
 						disabled={endIndex >= posts.length}
 					>
