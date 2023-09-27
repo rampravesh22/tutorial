aa7efaf64e10bf47384dfc8e3d6d47f19e75cf8e
rpc1752 <rpc1752@gmail.com>
2023-09-27 10:03:36 +0530

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,6 +3,7 @@ import { useGlobalContext } from "../context/contextapi";
 import Product from "./Product";
 
 const Products = () => {
+	const [addToCartNotification, setAddToCartNotification] = useState(false);
 	const { products } = useGlobalContext();
 	const [currentPage, setCurrentPage] = useState(1);
 	const dataPerPage = 6;
@@ -21,9 +22,36 @@ const Products = () => {
 
 	return (
 		<div className="main">
+			<div
+				className={` ${
+					addToCartNotification ? "scale-y-100" : "scale-y-0"
+				} origin-top fixed top-0 w-full z-50 add-t-cart-notification transition-all bg-emerald-700 h-12 flex items-center justify-between px-5 text-l font-light text-white`}
+			>
+				<p>Product added to cart successfully</p>
+				<div className="cart-btns flex gap-6">
+					<button
+						type="button"
+						className="shadow-md bg-gray-400 px-5 py-1 rounded-md"
+					>
+						Go to cart
+					</button>
+					<button
+						type="button"
+						onClick={() => setAddToCartNotification(false)}
+						className="shadow-md bg-red-400 px-5 py-1 rounded-md"
+					>
+						Close
+					</button>
+				</div>
+			</div>
 			<div className="product-wrapper w-[80%] m-auto p-10  flex flex-wrap gap-10 justify-center mt-4">
 				{paginateData.map((product) => (
-					<Product key={product.id} product={product} />
+					<Product
+						key={product.id}
+						product={product}
+						addToCartNotification={addToCartNotification}
+						setAddToCartNotification={setAddToCartNotification}
+					/>
 				))}
 			</div>
 			<div className="paginate-container items-center gap-4  flex mb-10 justify-center">
