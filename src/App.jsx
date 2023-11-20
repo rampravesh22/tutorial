5d040a063b2f274b20d101729ff97ae027990f7b
rpc1752 <rpc1752@gmail.com>
2023-11-20 07:42:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,112 @@
-function App() {
+const App = () => {
 	return (
-		<>
-			<div className="">App</div>
-		</>
+		<div className="flex border">
+			<div className="flex-grow flex flex-col">
+				<div className="bg-yellow-500 h-12 border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-red-500 h-12 sticky top-20 border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+				<div className="bg-yellow-500 h-12  border-black border"></div>
+			</div>
+			<div className="w-1/2">
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5 bg-blue-700 sticky top-0 border-blue-500">
+					left
+				</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+				<div className="left border-2 h-5  border-red-500">left</div>
+			</div>
+		</div>
 	);
-}
+};
 
 export default App;
