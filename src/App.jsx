5f3e7538d36e7d552cff421855b1ca28e137852d
rpc <rpc7863@gamil.com>
2023-11-25 20:52:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+
 function App() {
+	const [toggle, setToggle] = useState(false);
 	return (
-		<>
-			<div>Main</div>
-		</>
+		<div
+			style={{
+				position: "relative",
+				height: "100vh",
+				border: "3px solid red",
+			}}
+		>
+			<button
+				style={{
+					position: "absolute",
+					backgroundColor: "red",
+					right: ".1rem",
+					padding: ".3rem 1rem",
+					textAlign: "center",
+					width: "10rem",
+					borderRadius: ".5rem",
+					fontSize: "2rem",
+					fontFamily: "fantasy",
+					color: "white",
+					textTransform: "uppercase",
+					letterSpacing: ".1rem",
+				}}
+				onClick={() => setToggle(!toggle)}
+			>
+				Toggle
+			</button>
+			<div
+				className="sidebar"
+				style={{ height: "90%", background: "black", width: "40%" }}
+			></div>
+		</div>
 	);
 }
 
