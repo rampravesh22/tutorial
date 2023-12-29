8244d125f3b0b47b3c01653b3f77af4e08014e3d
rpc1752 <rpc1752@gmail.com>
2023-12-29 07:32:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 function App() {
 	return (
 		<>
-			<div className="">App</div>
+			<div
+				className="bg-red-500 w-[400px] h-[400px] flex justify-center items-center"
+				onClick={() => {
+					console.log("outer");
+				}}
+			>
+				<div
+					className="bg-black w-[300px] h-[300px] flex justify-center items-center"
+					onClick={() => console.log("inner 1")}
+				>
+					<div
+						className="bg-yellow-500 w-[200px] h-[200px]"
+						onClick={() => console.log("inner 2")}
+					></div>
+				</div>
+			</div>
 		</>
 	);
 }
