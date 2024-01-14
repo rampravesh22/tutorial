4e153d021ea9bb88c98c363e0ced0c277230e725
rpc1752 <rpc1752@gmail.com>
2024-01-14 20:35:36 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
+const count = 0;
 function App() {
+	const [randomNumber, setRandomNumber] = useState(
+		Math.floor(Math.random() * 10)
+	);
+	const [value, setValue] = useState(0);
+
+	const handleClick = () => {
+		if (value === randomNumber) {
+			setSuccess(true);
+		}
+	};
+	const [success, setSuccess] = useState(false);
+
 	return (
 		<>
-			<div className="">App</div>
+			<div className="flex justify-center items-center h-screen">
+				<div className="h-1/3 flex-col rounded w-1/2 flex justify-center gap-4 items-center shadow border">
+					<label htmlFor="number">Guess the number</label>
+					<input
+						type="number"
+						id="number"
+						value={value}
+						onChange={(e) => setValue(e.target.value)}
+						className="border text-center w-32 text-3xl rounded-md "
+					/>
+					<button
+						onClick={handleClick}
+						className="bg-green-700 rounded text-white px-6 py-1"
+					>
+						Check
+					</button>
+					{success && (
+						<div className="bg-pink-600 text-white px-4 py-1">
+							Successfully guessed
+						</div>
+					)}
+				</div>
+			</div>
 		</>
 	);
 }
