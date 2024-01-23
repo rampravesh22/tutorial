976d980059e9ca82034ea5319c0a12aef02dacc1
rpc1752 <rpc1752@gmail.com>
2024-01-23 13:23:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect } from "react";
+import supabase from "./supabaseClient";
+import { useState } from "react";
+
 function App() {
-	const name = 10;
+	const [todos, setTodos] = useState([]);
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				let { data: todos, error } = await supabase
+					.from("todo")
+					.select("*");
+				console.log("todo", todos);
+				console.log("error", error);
+				setTodos(todos);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getData();
+	}, []);
 	return (
 		<>
-			<div className="">App</div>
+			<div className="bg-red-500 text-center py-5">
+				{JSON.stringify(todos)}
+			</div>
 		</>
 	);
 }
