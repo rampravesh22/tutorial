9f10df48c519030acb09ba5be2458467e8f8c226
rpc1752 <rpc1752@gmail.com>
2024-02-03 11:53:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 function App() {
 	return (
 		<>
-			<div className="grid grid-cols-1 items-center content-center sm:grid-cols-2   lg:grid-cols-4 border-2 justify-center flex-wrap  p-1 m-2 border-black ">
-				<div className="w-[300px]  h-[300px] border bg-red-500"></div>
-				<div className="w-[300px] h-[300px] border bg-green-500"></div>
-				<div className="w-[300px] h-[300px] border bg-black"></div>
-				<div className="w-[300px]  h-[300px] border bg-blue-500"></div>
-				<div className="w-[300px] h-[300px] border bg-red-500"></div>
-				<div className="w-[300px] h-[300px] border bg-pink-500"></div>
-				<div className="w-[300px] h-[300px] border bg-yellow-500"></div>
-				<div className="w-[300px] h-[300px] border bg-slate-600"></div>
-				<div className="w-[300px] h-[300px] border bg-red-500"></div>
+			<div className="flex  flex-wrap justify-center">
+				<div className="sm:w-[200px] w-[80%]  h-[200px] border bg-red-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-green-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-black"></div>
+				<div className="sm:w-[200px] w-[80%]  h-[200px] border bg-blue-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-red-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-pink-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-yellow-500"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-slate-600"></div>
+				<div className="sm:w-[200px] w-[80%] h-[200px] border bg-red-500"></div>
 			</div>
 		</>
 	);
