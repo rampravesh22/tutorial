3e21eb09d9f6fb8a40c9f3ea9033094cb53fbf02
rpc1752 <rpc1752@gmail.com>
2024-02-28 10:56:34 +0530

    updated

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Modal = () => {
 	const [toggleModal, setToggleModal] = useState(false);
+	useEffect(() => {
+		document.body.style.overflow = toggleModal ? "hidden" : "auto";
+	}, [toggleModal]);
 	return (
 		<div>
+			{/* modal open button */}
 			<div className="flex justify-center items-center mt-4">
 				<button
 					onClick={() => setToggleModal(true)}
@@ -12,17 +16,40 @@ const Modal = () => {
 					Open
 				</button>
 			</div>
+			{/* modal container */}
 			<div
-				onClick={(e) =>
-					setToggleModal(
-						!(e.target === document.getElementById("editModalContainer"))
-					)
-				}
-				id="editModalContainer"
-				className="fixed inset-0  flex justify-center items-center"
+				onClick={(e) => {
+					if (e.target === document.getElementById("editModalContainer")) {
+						setToggleModal(false);
+					}
+				}}
+				className={`fixed inset-0 bg-[rgb(0,0,0,.5)] ${
+					toggleModal ? "scale-100" : "scale-0"
+				} flex justify-center items-center`}
 			>
-				<div className="h-44 w-[400px] shadow-md rounded-md border-pink-500 border flex flex-col p-3 bg-white text-black opacity-100">
-					<h1 className="text-2xl text-center mb-4">Edit Todo</h1>
+				{/* modal body */}
+				<div
+					className={`h-64 w-[400px] ${
+						toggleModal ? "scale-100" : "scale-90"
+					} shadow-md transition-transform rounded-md justify-between border-pink-500 border flex flex-col p-3 bg-white text-black opacity-100`}
+				>
+					<h1 className="text-2xl text-center mb-4">Modal Body</h1>
+					<div className="space-x-3 text-end">
+						{/* close button */}
+						<button
+							onClick={(e) => {
+								console.log("close button");
+								e.stopPropagation();
+								setToggleModal(false);
+							}}
+							className="self-center bg-blue-600 text-white px-5 py-1 rounded-md"
+						>
+							Close
+						</button>
+						<button className="self-center bg-green-600 text-white px-5 py-1 rounded-md">
+							Submit
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
