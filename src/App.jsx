3c55cae2b216eae543bc64736278761f2163713d
rpc1752 <rpc1752@gmail.com>
2024-03-06 08:36:54 +0530

    select input handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function App() {
+	const [course, setCourse] = useState("Math");
+	const handleInputChange = (e) => {
+		console.log(e.target.value);
+	};
 	return (
 		<>
-			<div className="">
-				<form>
-					<select name="course" id="course">
+			<div className="flex justify-center">
+				<form className="w-40">
+					<select
+						name="course"
+						onChange={handleInputChange}
+						value={course}
+						id="course"
+						className="w-full border border-black rounded-md py-1"
+					>
 						<option value="math">Math</option>
 						<option value="physics">Physics</option>
 						<option value="art">Art</option>
