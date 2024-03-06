bc9a784f3d9862af6961d6ec0f05a89105e9fe6a
rpc1752 <rpc1752@gmail.com>
2024-03-06 08:12:15 +0530

    checkbox handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 function App() {
+	const [gender, setGender] = useState("Male");
+	const handleInputChange = (e) => {
+		setGender(e.target.value);
+	};
+	console.log(gender);
 	return (
 		<>
-			<div className="">App</div>
+			<div className="">
+				<form action="" className="flex gap-4">
+					<span>Gender</span>
+					<label htmlFor="male">Male</label>
+					<input
+						onChange={handleInputChange}
+						type="radio"
+						name="gender"
+						id="male"
+						value="Male"
+						checked={gender === "Male"}
+					/>
+					<label htmlFor="female">Female</label>
+					<input
+						onChange={handleInputChange}
+						type="radio"
+						name="gender"
+						id="female"
+						value="Female"
+						checked={gender === "Female"}
+					/>
+					<label htmlFor="other">Other</label>
+					<input
+						onChange={handleInputChange}
+						type="radio"
+						name="gender"
+						id="other"
+						value="Other"
+						checked={gender === "Other"}
+					/>
+				</form>
+			</div>
 		</>
 	);
 }
