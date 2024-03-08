69e109ed4225c74eb0e6e9bc7376e87823fc6221
rpc1752 <rpc1752@gmail.com>
2024-03-08 16:03:35 +0530

    first commit in react-oauth-google

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-function App() {
+import { useGoogleLogin } from "@react-oauth/google";
+
+const App = () => {
+	const login = useGoogleLogin({
+		onSuccess: (tokenResponse) => console.log(tokenResponse),
+	});
 	return (
 		<>
-			<div className="">App</div>
+			<div className="h-screen bg-gray-200 w-full flex justify-center items-center">
+				<div className="bg-white h-64 w-96 rounded-md justify-center flex">
+					<button
+						className="bg-blue-600 w-40 m-auto text-white py-2 rounded"
+						onClick={login}
+					>
+						Login
+					</button>
+				</div>
+			</div>
 		</>
 	);
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+			<App />
+		</GoogleOAuthProvider>
+	</React.StrictMode>
+);
