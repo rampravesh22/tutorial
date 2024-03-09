6bb0bca732a3cd869d4950c64f14008c2642a858
rpc1752 <rpc1752@gmail.com>
2024-03-09 16:26:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+
 function App() {
+	useEffect(() => {
+		gsap.to("#box", {
+			x: 600,
+			delay: 3,
+			rotate: 360,
+			duration: 5,
+			scrollTrigger: true,
+		});
+	}, []);
 	return (
 		<>
-			<div className="">App</div>
+			<div
+				id="box"
+				className="h-40 mt-10 w-40 rounded-full bg-red-600 after:w-full after:block after:rounded-t-full after:h-20 after:bg-black after:z-10 "
+			></div>
 		</>
 	);
 }
