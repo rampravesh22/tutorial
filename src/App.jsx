705dbf1868643d14ce1cbc59b265343910524a31
rpc1752 <rpc1752@gmail.com>
2024-03-30 10:02:27 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
 	return (
-		<>
-			<div className="">App</div>
-		</>
+		<div className="flex flex-col h-screen">
+			<Header />
+			<main className="flex-grow"></main>
+			<Footer />
+		</div>
 	);
 }
 
