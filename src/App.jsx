3fe56517867bc74e0f60b8afe035dc4a882c78bb
rpc1752 <rpc1752@gmail.com>
2024-03-31 10:23:34 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import gsap from "gsap";
 function App() {
 	useEffect(() => {
 		gsap.to("#box", {
-			x: 600,
-			delay: 3,
-			rotate: 360,
-			duration: 5,
-			scrollTrigger: true,
+			delay: 1,
+			scale: 3,
+			duration: 1,
+			backgroundColor: "blue",
 		});
 	}, []);
 	return (
-		<>
+		<div className="h-screen bg-gray-300 flex justify-center items-center">
 			<div
 				id="box"
 				className="h-40 mt-10 w-40 rounded-full bg-red-600 after:w-full after:block after:rounded-t-full after:h-20 after:bg-black after:z-10 "
 			></div>
-		</>
+		</div>
 	);
 }
 
