18c21456f90e97bda999d53ceb3bd1f7bc0741b0
rpc1752 <rpc1752@gmail.com>
2024-04-01 08:52:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import gsap from "gsap";
-
 function App() {
 	useEffect(() => {
 		gsap.to("#box", {
-			delay: 4,
+			delay: 2,
 			scale: 5,
 			duration: 5,
 			x: 200,
 		});
 	}, []);
 	return (
-		<div className="h-screen bg-gray-300  flex justify-center items-center">
-			<div id="box" className="box w-20 mt-12 size-5  bg-red-600"></div>
+		<div className="h-screen bg-gray-300  flex items-center">
+			<div id="box" className="mt-12 size-10  bg-red-600"></div>
 		</div>
 	);
 }
-
 export default App;
