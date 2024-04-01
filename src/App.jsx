5494e1ceff79bbb9842921a1901a3a17c81c1fd5
rpc1752 <rpc1752@gmail.com>
2024-04-01 09:02:41 +0530

    upadted

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,11 @@ function App() {
 			delay: 1,
 			scale: 3,
 			duration: 1,
-			backgroundColor: "blue",
 		});
 	}, []);
 	return (
-		<div className="h-screen bg-gray-300 flex justify-center items-center">
-			<div
-				id="box"
-				className="h-40 mt-10 w-40 rounded-full bg-red-600 after:w-full after:block after:rounded-t-full after:h-20 after:bg-black after:z-10 "
-			></div>
+		<div className="h-screen bg-gray-300 flex items-center">
+			<div id="box" className="size-10 ml-10 bg-pink-700"></div>
 		</div>
 	);
 }
