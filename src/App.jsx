dbfcfaecf0e265ff41c8cdfc30b257120188bf48
rpc1752 <rpc1752@gmail.com>
2024-04-01 10:26:00 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useEffect } from "react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 
 function App() {
 	useEffect(() => {
-		gsap.from("#box1", {
-			delay: 1,
-			scale: 3,
+		gsap.to(".box", {
+			delay: 2,
+			scale: 4,
 			duration: 2,
-			x: 300,
+			x: 204,
+			stagger: 4,
+			repeat: 2,
 		});
 	}, []);
 	return (
-		<div className="h-screen bg-gray-300  flex items-center">
-			<div id="box1" className="size-10 ml-10 bg-pink-700"></div>
-			<div id="box2" className="size-10 ml-10 bg-gray-700"></div>
+		<div className="h-screen  bg-gray-300 gap-32 flex-col flex justify-center">
+			<div id="box1" className="box size-10 ml-20  bg-pink-700"></div>
+			<div id="box2" className="box size-10 ml-20  bg-gray-700"></div>
 		</div>
 	);
 }
