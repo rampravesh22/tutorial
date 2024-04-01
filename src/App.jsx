54e70fb4541178e4c02b6649f150d10d19251109
rpc1752 <rpc1752@gmail.com>
2024-04-01 08:54:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ function App() {
 	useEffect(() => {
 		gsap.to("#box", {
 			delay: 1,
-			scale: 3,
+			scale: 1,
 			duration: 1,
 			backgroundColor: "blue",
 		});
 	}, []);
 	return (
-		<div className="h-screen bg-gray-300 flex justify-center items-center">
-			<div
-				id="box"
-				className="h-40 mt-10 w-40 rounded-full bg-red-600 after:w-full after:block after:rounded-t-full after:h-20 after:bg-black after:z-10 "
-			></div>
+		<div className="h-screen bg-gray-300 flex  items-center">
+			<div id="box" className="size-20"></div>
 		</div>
 	);
 }
