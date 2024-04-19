c51f06764268329d25c8ef4efaa1c033e3efa7c6
rpc1752 <rpc1752@gmail.com>
2024-04-19 15:50:24 +0530

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,33 +1,21 @@
 import React from "react";
 
 const Home = () => {
+	const numbers = Array(200).fill(1);
 	return (
-		<div className="flex flex-col gap-2 p-2">
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
-			<div className=" h-32 border border-gray-800  rounded bg-gray-300"></div>
+		<div className="grid sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-3 xl:grid-cols-4  gap-2 p-5">
+			{numbers.map((val, index) => {
+				return (
+					<div
+						key={index}
+						className="overflow-hidden p-5  h-44 border border-gray-800  rounded bg-gray-300"
+					>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+						eum laudantium est itaque sunt expedita distinctio atque cumque
+						vitae eveniet.
+					</div>
+				);
+			})}
 		</div>
 	);
 };
