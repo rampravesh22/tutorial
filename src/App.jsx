c386150fe501a57d32ccb75e48fe154310379bbc
rpc1752 <rpc1752@gmail.com>
2024-04-30 11:53:32 +0530

    font-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 function App() {
-	return (
-		<>
-			<div className="mt-20 text-5xl font-montserrat selection:bg-green-600 selection:text-white p-2 first-letter:text-[100px]">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem
-				exercitationem, qui est consectetur corrupti veritatis blanditiis alias
-				sapiente ducimus ullam repellendus quaerat beatae dignissimos atque
-				ipsam molestias voluptatibus porro.
-			</div>
-		</>
-	);
+	return <div>App</div>;
 }
 
 export default App;
