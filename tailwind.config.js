6fb7831ef3a923c5460eee69ebf41c6d82f9745f
rpc1752 <rpc1752@gmail.com>
2024-04-30 11:52:47 +0530

    fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,jsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			"noto-sans": ["Noto Sans", "sans-serif"],
+			roboto: ["Roboto", "sans-serif"],
+			"open-sans": ["Open Sans", "sans-serif"],
+			montserrat: ["Montserrat", "sans-serif"],
+			"nunito-sans": ["Nunito Sans", "sans-serif"],
+			nunito: ["Nunito", "sans-serif"],
+			oswald: ["Oswald", "sans-serif"],
+			"roboto-mono": ["Roboto Mono", "sans-serif"],
+			"roboto-condensed": ["Roboto Condensed", "sans-serif"],
+			inter: ["Inter", "sans-serif"],
+			poppins: ["Poppins", "sans-serif"],
+			lato: ["Lato", "sans-serif"],
+			ubuntu: ["Ubuntu", "sans-serif"],
+		},
 	},
+
 	plugins: [],
 };
