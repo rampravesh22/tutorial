6f1051a42a83ee86609149654604bd5ad6ba4daf
rpc1752 <rpc1752@gmail.com>
2024-05-02 09:58:41 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,200 @@ function App() {
 			<div className="">
 				<Navbar />
 				<div className="p-2 space-y-4">
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
-					<div className="h-32 border-2 border-black"></div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+				</div>
+				<div className="p-2 space-y-4">
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
+					<div className="h-32 border-2 border-black p-2 rounded">
+						<h1 className="text-2xl underline">Lorem ipsum dolor sit amet.</h1>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+							voluptas cum eaque officia beatae quibusdam voluptatum sint unde
+							laboriosam quasi!
+						</p>
+					</div>
 				</div>
 			</div>
 		</>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,21 +5,21 @@ import { LuMenu } from "react-icons/lu";
 const Navbar = () => {
 	const [menu, setMenu] = useState(false);
 	return (
-		<div className="bg-blue-800 relative  text-white flex z-20 items-center px-5 h-12">
-			<div className="flex w-full justify-between items-center">
-				<div className="text-xl z-30">Logo</div>
+		<div className="bg-blue-800 relative  text-white flex z-40 items-center px-5 h-12">
+			<div className="flex w-full z-50 justify-between items-center">
+				<div className="text-xl ">Logo</div>
 				<button
 					onClick={() => setMenu(!menu)}
-					className="flex z-30 text-2xl items-center"
+					className="flex z-50 text-2xl items-center"
 				>
 					{menu ? <CgClose className="" /> : <LuMenu />}
 				</button>
 			</div>
 			<div
-				onClick={() => setMenu(false)}
-				className={`absolute z-10 top-12 flex transition-all justify-center h-96  pt-10 duration-500 right-0 w-full ${
+				// onClick={() => setMenu(true)}
+				className={`content absolute  flex transition-all justify-center h-96  pt-10 duration-500 right-0  -z-10 ${
 					menu ? "top-12" : "-top-96"
-				} bg-blue-800 left-0`}
+				} bg-blue-700 left-0`}
 			>
 				<ul className="flex -z-0 flex-col gap-7 text-3xl">
 					<li>
