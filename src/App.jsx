f02e57aadaa0f86407431d5ab1b6817075fc5d51
rpc1752 <rpc1752@gmail.com>
2024-06-18 16:27:50 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="h-[100svh] flex justify-center items-center">
+			<div className="parent relative z-10 size-[400px]   bg-blue-800">
+				<div className="absolute  child -z-20 bg-black size-[200px]"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
