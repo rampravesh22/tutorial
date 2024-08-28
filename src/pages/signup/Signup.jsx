38b520e28e781df9acf1190d9ce2ca22871a7de8
rpc1752 <rpc1752@gmail.com>
2024-08-28 09:58:13 +0530

    updated

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { button, Button, Input } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 import { supabase } from "../../supabaseClient";
 import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 const Signup = () => {
@@ -7,13 +7,14 @@ const Signup = () => {
 	const toggleVisibility = () => setIsVisible(!isVisible);
 	const [loading, setLoading] = useState(false);
 	const [formData, setFormData] = useState({
-		email: "",
-		password: "",
+		email: "rickwart1@gmail.com",
+		password: "toofanram",
 	});
 	const handleRegisterSumbit = async (e) => {
 		e.preventDefault();
 		try {
 			setLoading(true);
+			console.log(formData);
 			const { data, error } = await supabase.auth.signUp({
 				email: formData.email,
 				password: formData.password,
@@ -45,7 +46,6 @@ const Signup = () => {
 			<Input
 				type="text"
 				errorMessage="this is not correct"
-				// labelPlacement="outside"
 				label="Email"
 				value={formData.email}
 				onChange={handleInputChange}
@@ -54,7 +54,6 @@ const Signup = () => {
 			<Input
 				type={isVisible ? "text" : "password"}
 				errorMessage="this is not correct"
-				// labelPlacement="outside"
 				label="Password"
 				value={formData.password}
 				name="password"
