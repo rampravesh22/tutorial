ca9bc6ee7a88565d8f07e05d76bd9986fc448af5
rpc1752 <rpc1752@gmail.com>
2024-09-14 19:04:02 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { useEffect } from "react";
+import { API_KEY } from "./api";
+
 function App() {
-	return <div>App</div>;
+	useEffect(() => {
+		const genAI = new GoogleGenerativeAI(API_KEY);
+		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+		const prompt = "Write a story about a magic backpack.";
+
+		model
+			.generateContent(prompt)
+			.then((res) => {
+				console.log(res.response.text);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}, []);
+	return <div>{"hello"}</div>;
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,1 @@
+export const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
