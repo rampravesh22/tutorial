bfd920f44bd66508752b9de8ff6e39c15a582a21
rpc1752 <rpc1752@gmail.com>
2024-09-24 20:07:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-	return <div>App</div>;
+	return (
+		<div className="w-full  h-24 bg-black">
+			<button className="text-white sm:hidden">click me</button>
+		</div>
+	);
 }
 
 export default App;
