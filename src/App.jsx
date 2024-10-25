be33d15d73996cab2f6d118cfb1254c59b49e2fd
rpc1752 <rpc1752@gmail.com>
2024-10-25 15:15:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const PrivatRoute = ({ isAuthenticated, children }) => {
+	return isAuthenticated ? children : <Login />;
+};
 
 function App() {
 	return (
@@ -8,6 +14,8 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route path="/" element={<Home />} />
+					<Route path="register" element={<Register />} />
+					<Route path="login" element={<Login />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
