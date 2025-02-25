62260a44b33e37b902d29b6da42963be3e11726f
rpc1752 <rpc1752@gmail.com>
2025-02-25 12:51:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 function App() {
-	return <div>App</div>;
+	return (
+		<div
+			className="h-80 bg-red-700 justify-center items-center flex"
+			onClickCapture={() => console.log("grandparent")}
+		>
+			<div
+				className="h-64 bg-white w-[90%] flex justify-center items-center"
+				onClickCapture={() => console.log("parent")}
+			>
+				<div
+					className="flex justify-center items-center w-[90%] h-[80%] bg-purple-700"
+					onClickCapture={(e) => {
+						console.log("chiled");
+					}}
+				>
+					<div
+						className="bg-black h-[60%] w-[80%]"
+						onClickCapture={(e) => {
+							console.log("grandchild");
+						}}
+					></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
